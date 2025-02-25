6d30eb61ab7fdd369c2afe350fe940a9f244f376
sumeet <maheshwarisumeet128@gmail.com>
2025-02-26 00:06:34 +0530

    creating the utils functions

--- llamaindex_agents/Multi-Agent Research Workflow with AgentWorkflow/utils.py
+++ llamaindex_agents/Multi-Agent Research Workflow with AgentWorkflow/utils.py
@@ -1,4 +1,7 @@
 from llama_index.llms.groq import Groq
+from llama_index.core.workflow import Context
+from tavily import AsyncTavilyClient
+
 from dotenv import load_dotenv
 import os
 import logging
@@ -6,6 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 GROQ_API = os.getenv("GROQ_API")
+tavily  = os.getenv("TAVILY")
 
 
 class LLM:
@@ -22,5 +26,26 @@ class LLM:
             logging.error(f"Error in initializing LLM model: {e}")
 
 
+class Utils:
+    def __init__(self):
+        logging.info("Initializing Utils")
 
-        
+    async def search_web(query:str)-> str:
+        """
+        useful for using the web to answer the queries
+        """
+        client = AsyncTavilyClient(api_key=tavily)
+        return str(await client.search(query))
+    
+    async def record_notes(ctx: Context, notes:str, notes_title:str,) -> str:
+        """
+        useful for recording the notes
+        """
+        current_states = await ctx.get("state")
+        if "research_notes" not in current_states:
+            current_states["research_notes"] ={}
+        current_states["research_notes"][notes_title] = notes
+        await ctx.get("state", current_states)
+        return "Notes recorded successfully"
+    
+    
